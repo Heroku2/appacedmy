2b78412cd5d4f977288c4b972435f0b7f14ad2b7
JOBFER <gbringashotmail@hotmail.com>
2017-10-08 10:42:47 +0200

    Inicio

--- src/main/java/com/example/demo/controller/TeacherController.java
+++ src/main/java/com/example/demo/controller/TeacherController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -30,13 +31,14 @@ public class TeacherController {
 	
 	
 	@PostMapping("/teachers")
-	public String save(@Valid Teacher teacher, BindingResult result){
+	public String save(@ModelAttribute("teacher") @Valid Teacher teacher, BindingResult result, Model model){
+		
 		if(result.hasErrors()){
-			return "redirect:/teachers";  //Por GET
+			model.addAttribute("teachers", teacherService.findAll());
+			return "Teacher";
+//			return "redirect:/teachers";  //Por GET
 		}
-		
 		this.teacherService.save(teacher);
-		
 		return "redirect:/teachers"; //Por GET
 	}
 	
